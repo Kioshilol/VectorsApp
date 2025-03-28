a33d12d9b899b4c2996856d8cc0820408122307e
Pavel <48092890+Kioshilol@users.noreply.github.com>
2025-03-28 19:06:16 +0300

    release 1.0.0 (#2)
    * wip
    
    * added core data
    
    * fixed bugs, added horizontal/vertical stick
    
    * Create README.md (#1)
    
    * added long press logic
    
    * Added sticking end of vector to another end of vector
    
    * update readme
    
    * added right angle stick logic
    
    ---------
    
    Co-authored-by: Pavel <Khmylko>

---
+++ VectorEntity+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  VectorEntity+CoreDataProperties.swift
+//  Vectors
+//
+//  Created by paintmethecolorofchaos on 24.03.25.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+
+extension VectorEntity {
+    @NSManaged var uuid: UUID
+    @NSManaged var startX: Double
+    @NSManaged var endX: Double
+    @NSManaged var startY: Double
+    @NSManaged var endY: Double
+    @NSManaged var name: String
+    @NSManaged var color: UIColor
+    
+    //TODO: for sitcked vector create entity
+    @NSManaged var stickedPosition: Int16
+    @NSManaged var stickedVectorUuid: UUID?
+    @NSManaged var stickedVectorPosition: Int16
+}
+
+extension VectorEntity : Identifiable {
+
+}
